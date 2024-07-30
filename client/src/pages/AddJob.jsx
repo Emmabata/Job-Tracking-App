4e27ae7c313b318fb750a2741a21cd929533f664
Emmabata <ndinezeemmanuel@gmail.com>
2024-07-30 12:52:27 +0100

    job add structure

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,10 +1,47 @@
+import { FormRow, FormRowSelect } from '../components';
+import Wrapper from '../assets/wrappers/DashboardFormPage';
+import { Form, redirect, useNavigation, useOutletContext } from 'react-router-dom';
+import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
+import { toast } from 'react-toastify';
+import customFetch from '../utils/customFetch';
+
+
 
 const AddJob = () => {
+    const { user } = useOutletContext();
+    const navigation = useNavigation();
+    const IsSubmitting = navigation.state === "submitting";
     return ( 
-        <div>
-            <h1>Add Job</h1>
-        </div>
+        <Wrapper>
+            <Form>
+                <h4 method='post' className='form'>add job</h4>
+                <div className='form-center'>
+                    <FormRow type='text' name='position' />
+                    <FormRow type='text' name='company' />
+                    <FormRow type='text' labelText='job location' name='jobLocation' defaultValue={user.location} />
+                    <FormRowSelect 
+                        labelText='job status'
+                        name='jobStatus'
+                        defaultValue={JOB_STATUS.PENDING}
+                        list={Object.values(JOB_STATUS)}
+                        />
+                    <FormRowSelect 
+                        labelText='job type'
+                        name='jobType'
+                        defaultValue={JOB_TYPE.FULL_TIME}
+                        list={Object.values(JOB_TYPE)}
+                        />
+                    <button 
+                        type='submit'
+                        className='btn btn-block form-btn'
+                        disabled={IsSubmitting}
+                        > 
+                        {IsSubmitting ? 'submitting' : "submit"}
+                        </button>
+                </div>
+            </Form>
+        </Wrapper>
     );
-}
+};
 
 export default AddJob;
