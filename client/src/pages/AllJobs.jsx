c9f4ce50cfd60a02a65a422d0c1262154397d7d8
Emmabata <ndinezeemmanuel@gmail.com>
2024-08-09 17:03:15 +0100

    Job Container

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -8,17 +8,28 @@ import { useContext, createContext } from "react";
 
 export const loader = async () => {
     try{
-
+        const {data} = await customFetch.get('/jobs');
+        return {data}
     }catch (error) {
-        toast.error()
+        toast.error(error?.response?.data?.msg)
+        return error
     }
-}
+};
+
+const AllJobsContext = createContext();
 
 
 const AllJobs = () => {
+    const {data} = useLoaderData()
+
     return (
-        <h1>All Jobs</h1>
+        <AllJobsContext.Provider value={{data}}>
+        <SearchContainer />
+        <JobsContainer />
+        </AllJobsContext.Provider>
     );
-}
+};
+
+export const useAllJobsContext = () => useContext(AllJobsContext);
 
 export default AllJobs;
